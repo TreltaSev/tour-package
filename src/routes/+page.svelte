4fb0695517db13181445d3ae0a8b94edfc1c15f9
TreltaSev <treltasev@gmail.com>
2025-02-24 18:35:22 -0500

    Amazing update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import Mpaodwnij from '@root/lib/components/mpaodwnij/components/mpaodwnij.svelte';
 	import { Page, Span } from '@ui';
 </script>
 
 <Page.Root>
-	<Span>Testing</Span>
-	<Mpaodwnij statement="Amazing statement"/>
+	<Span>Testing This is an amazing uypdate</Span>
 </Page.Root>
