09cf6647f6885a9a6a8846293ecb8a75297c50ca
Raptor12762 <sethmwhitman7@gmail.com>
2025-02-26 17:08:01 -0500

    tailwind in place of inline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <Page.Root>
-	<!-- This is the actual Page Content -->
-	 <h1 style="text-align:center; font-size:96px;">MAUI</h1>
-	 <!--need to create a button linking to the form we create next to the header (or wherever we can in the top right of the page) that just stays in place-->
-	 <hr style="border:1px solid white;">
-		<!--insert main page content starting here-->
+    <!-- This is the actual Page Content -->
+     <h1 class="text-center text-8xl">MAUI</h1>
+     <!--need to create a button linking to the form we create next to the header (or wherever we can in the top right of the page) that just stays in place-->
+     <hr class="border border-solid border-white">
+        <!--insert main page content starting here-->
 </Page.Root>
