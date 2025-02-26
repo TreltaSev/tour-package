aa5a3cde783e854f287a2987df47eb0d1838e1f9
Raptor12762 <sethmwhitman7@gmail.com>
2025-02-26 16:53:51 -0500

    Main page based off design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,8 @@
 
 <Page.Root>
 	<!-- This is the actual Page Content -->
-	<Span>This is a website colonD</Span>	
+	 <h1 style="text-align:center; font-size:96px;">MAUI</h1>
+	 <!--need to create a button linking to the form we create next to the header (or wherever we can in the top right of the page) that just stays in place-->
+	 <hr style="border:1px solid white;">
+		<!--insert main page content starting here-->
 </Page.Root>
