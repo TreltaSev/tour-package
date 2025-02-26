f826d7c33978266ef4f28683d9230fdaeae5380f
Trelta Sebastion <treltasev@gmail.com>
2025-02-26 16:37:17 -0500

    Guideline Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { Flex, Page, Span } from '@ui';
+
+	// This is where functional logic is, for main pages this might not be used
 </script>
 
 <Page.Root>
+	<!-- This is the actual Page Content -->
 	<Span>This is a website colonD</Span>	
 </Page.Root>
