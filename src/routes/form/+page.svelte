8c12a9e52f8162fd3d7830424d061e4f0c6ced60
Raptor12762 <sethmwhitman7@gmail.com>
2025-02-26 21:44:09 -0500

    missing <br> tags

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -10,7 +10,7 @@
     <label for="last" style="color: white;">Last Name: </label>
     <input type="text" id="last" placeholder="Insert your Last Name here" style="color: white;" size="25">
     <!--First Name and Last Name Fields-->
-    <br>
+    <br><br>
     <!--Party Size-->
     <label for="party-size" style="color: white;">Number of people in party:</label>
     <select id="party-size" name="party-size" style="color: white;">
@@ -23,18 +23,18 @@
     <label for="Tour-Package" style="color:white;">Select Tour Package:</label>
     <select id="Tour-Package" name="Tour-Package" style="color: white;">
         <!--Fill out the options once we decide what we need to add-->
-        <option value="1" style="color: black;">1</option>
-        <option value="2" style="color: black;">2</option>
-        <option value="3" style="color: black;">3</option>
-        <option value="4" style="color: black;">4</option>
+        <option value="1tour" style="color: black;">1</option>
+        <option value="2tour" style="color: black;">2</option>
+        <option value="3tour" style="color: black;">3</option>
+        <option value="4tour" style="color: black;">4</option>
     </select>
-    <br>
+    <br><br>
     <label for="Air" style="color:white;">Include Air Travel: </label>
     <input type="radio" id="Yes" name="Air">
     <label for="Yes" style="color:white;">Yes</label>
     <input type="radio" id="No" name="Air">
     <label for="No" style="color:white;">No</label>
-    <br>
+    <br><br>
     <!--Options in package-->
     <label for="Options" style="color:white;">Select Options to include in package: </label>
     <input type="checkbox" id="meals" name="meals" value="meals">
@@ -43,6 +43,6 @@
     <label for="wine" style="color:white;">Evening Wine</label>
     <input type="checkbox" id="car" name="car" value="car">
     <label for="car" style="color:white;">Car Rental</label>
-    <br>
+    <br><br>
     <button type="submit" form="tour" value="Submit" style="color:white;">Process Request</button>
 </form>
