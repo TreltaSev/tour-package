49e94d72af21db209bec538cf51fd7e7f3a7ebef
TreltaSev <treltasev@gmail.com>
2025-03-01 16:37:57 -0500

    add/ base component

---
+++ src/lib/ui/fscrollable/index.ts
@@ -0,0 +1,5 @@
+export {default as _, default as Root} from "./components/fscrollable.svelte";
+
+export type {
+    tFscrollableProps as Props,
+} from "./types"
---
+++ src/lib/ui/fscrollable/types.ts
@@ -0,0 +1,11 @@
+import type { ClassValue, HTMLAttributes } from "svelte/elements";
+
+export type tFscrollableProps = HTMLAttributes<HTMLDivElement> & {
+    // Classes:
+
+    // --- Default Classes:
+    fscrollableClass?: ClassValue,
+    
+    // Extra Props Here:
+
+};
