baf94fd40e92a18e53283c340ebf4bed3bba4222
TreltaSev <treltasev@gmail.com>
2025-03-01 00:24:53 -0500

    content/ now animates on page load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,17 @@
 </script>
 
 <svelte:head>
-    <title>Main Page</title>
+	<title>Main Page</title>
 </svelte:head>
 
-<Page.Root>
-    <!-- This is the actual Page Content -->
-     <h1 class="text-center text-8xl">MAUI</h1>
-     <!--need to create a button linking to the form we create next to the header (or wherever we can in the top right of the page) that just stays in place-->
-     <hr class="border border-solid border-black dark:border-white">
-        <!--main page content, might need to make it nicer-->
-        <p style="text-align: center;">Welcome to Maui Travels. The only website you will ever need to tour Maui. Here, you will find all the activities you would ever want to do while in Maui. We offer a variety of tour packages, from the volcanoes, to the beaches. We will provide you with the information you need to have a fun, relaxing vacation with us.</p>
-</Page.Root>
+<!-- This is the actual Page Content -->
+<h1 class="text-center text-8xl">MAUI</h1>
+<!--need to create a button linking to the form we create next to the header (or wherever we can in the top right of the page) that just stays in place-->
+<hr class="border border-solid border-black dark:border-white" />
+<!--main page content, might need to make it nicer-->
+<p style="text-align: center;">
+	Welcome to Maui Travels. The only website you will ever need to tour Maui. Here, you will find all
+	the activities you would ever want to do while in Maui. We offer a variety of tour packages, from
+	the volcanoes, to the beaches. We will provide you with the information you need to have a fun,
+	relaxing vacation with us.
+</p>
