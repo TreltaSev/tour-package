6711e03e323902d02c053356cf6092711415124b
Raptor12762 <sethmwhitman7@gmail.com>
2025-03-06 12:56:41 -0500

    submit button in payment :)

--- src/routes/form/payment/+page.svelte
+++ src/routes/form/payment/+page.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
-	import { deserializeData } from "@root/lib/internal";
-	import { getPageCtx } from "@root/lib/ui/page/ctx";
-	import { Span } from "@ui";
+	import { deserializeData } from '@root/lib/internal';
+	import { getPageCtx } from '@root/lib/ui/page/ctx';
+	import { Span } from '@ui';
 
-    const data = deserializeData(window.location.search)
+	const data = deserializeData(window.location.search);
 
-    const { hideDrawer$ } = getPageCtx()
+	const { hideDrawer$ } = getPageCtx();
 
-    hideDrawer$.set(true)
+	hideDrawer$.set(true);
 </script>
 
 {#if !data}
-   <Span>No data</Span>
+	<Span>No data</Span>
 {:else}
-    <Span>{data['first-name']}</Span> 
+	<Span>{data['first-name']}</Span>
 {/if}
 
+<button class="text-white px-6 py-2 rounded-sm bg-primary-600 cursor-pointer">
+	Pay $<!--Insert custom payment amount based off selection here-->
+</button>
